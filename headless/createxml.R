1f2796dee6dbc15e5d292051eea0c0792d975783
M Salles <malicsalles@gmail.com>
2021-03-10 14:05:32 -0300

    scenarios

--- headless/createxml.R
+++ headless/createxml.R
@@ -1,69 +1,36 @@
 
 
-finalstep <- 1500
+finalstep <- 2000
 numreps <- 12
+samplesize<-3
 
-gamlfile <- 'C:\\Users\\marin\\Documents\\Academico\\Gama\\floresta-campo\\FireandForest\\models\\instafire.gaml'
-filename <- paste0("fc",finalstep,'_',numreps,".xml")
+## filenaming
+today <- paste0(strsplit(date()," ")[[1]][c(2:3,5)],collapse="")
+gamlfile <- '..\\FireandForest\\models\\instafire.gaml'
+filename <- paste0("LHS_",today,".xml")
 
 file.create(filename)
-w <- function (str) {
-    write(str,file=filename,append=T)
-}
-
 ## CONSTANTS
-
-header <- "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>
-<Experiment_plan>
-"
-outputs <- '
-	  <Outputs>
-	    <Output id="1" name="Number of araucaria trees" framerate="1" />
-	    <Output id="2" name="Number of broadleaved trees" framerate="1" />
-	    <Output id="3" name="Circle size for araucaria trees" framerate="1" />
-	    <Output id="4" name="Circle size for broadleaved trees" framerate="1" />
-	    <Output id="5" name="Size of fire" framerate="1" />
-	  </Outputs>'
-
-simheadbeg <- '
-    <Simulation id="'
-simheadend <- paste0('" sourcePath="',gamlfile,'" finalStep="',finalstep,'" experiment="fireandforest">')
-
-footer <- '</Experiment_plan>'
+source("createxml.utils.R")
 
 ## VARIABLES
+param.shared <- par.line('Shade tolerance ratio',2.0)
 
-FMparams <- '	  <Parameters>
-        <!-- standard parameters -->
-	  </Parameters>'
-
-
-NAparams <- '	  <Parameters>
-	    <Parameter name="Initial Araucaria pop" type="INT" value="0" />
-	  </Parameters>'
-
-      
-NFparams <- '	  <Parameters>
-	    <Parameter name="Wildfires" type="BOOLEAN" value="false" />
-	  </Parameters>'
+FMparams <- paste0(param.shared,'')
+NAparams <- paste0(param.shared,'<Parameter name="Initial Araucaria pop" type="INT" value="0" />')
+NFparams <- paste0(param.shared,'<Parameter name="Wildfires" type="BOOLEAN" value="false" />')
 
 my_names <- c("Full","NoAr","NoFi")
 my_params <- c(FMparams,NAparams,NFparams)
 
+## UNIQUE IDS
+sim_ids <- paste0(my_names,'_',today,'_',rndc)  ## array of sim ids
+simheaders <- paste0(simheadbeg,sim_ids,simheadend) ## array of headers
+
 ## WRITING
-      
+simxml <- paste(simheaders,my_params,outputs,'</Simulation>\n', sep="\n")
+
+file.create(filename)
 write(header, filename, append=FALSE)
-
-for(i in 1:numreps) {
-    for(type in 1:3) {
-        ## make sim header
-        sim_id <- paste0(my_names[type],i)
-        simheader <- paste0(simheadbeg,sim_id,simheadend)
-        w(simheader)
-        w(my_params[type])
-        w(outputs)
-        w('	</Simulation>')
-    }
-}
-
+sapply(simxml,FUN=w)
 w(footer)
