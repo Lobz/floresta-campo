c3f8bb106aaf93e71cb692c51d93d99c27e774f3
M Salles <malicsalles@gmail.com>
2021-03-12 16:36:35 -0300

    towards hyp testing

--- headless/analyse.R
+++ headless/analyse.R
@@ -10,13 +10,51 @@ data <- subset(datafull, shade.threshold.ratio > 0)
 
 plot_final_values(data,"wildfire_rate")
 
+## get one parameter set
+parcols <- 6:13 ## cols with parameter values
+NoArdata <- subset(data,initial_pop_araucaria==0)
+NoFidata <- subset(data,wildfire_rate==0)
+Fulldata <- subset(data,initial_pop_araucaria>0 & wildfire_rate >0)
+
+NoFi.one <- subset(NoFidata,sim_unique_id==NoFidata$sim_unique_id[1])
+NoAr.one <- subset(NoArdata, shade_threshold_ratio == NoFi.one$shade_threshold_ratio[1])
+Full.one <- subset(Fulldata, shade_threshold_ratio == NoFi.one$shade_threshold_ratio[1])
+
+## extract variables
+finaltime <- max(data$time)
+
+nA.NoFi.final <- subset(NoFi.one,time==finaltime)$n.araucaria
+nA.NoFi.initial <- subset(NoFi.one,time==1)$n.araucaria
+nA.NoAr.final <- subset(NoAr.one,time==finaltime)$n.araucaria
+nA.NoAr.initial <- subset(NoAr.one,time==1)$n.araucaria
+nA.Full.final <- subset(Full.one,time==finaltime)$n.araucaria
+nA.Full.initial <- subset(Full.one,time==1)$n.araucaria
+
+nB.NoFi.final <- subset(NoFi.one,time==finaltime)$n.broadleaf
+nB.NoFi.initial <- subset(NoFi.one,time==1)$n.broadleaf
+nB.NoAr.final <- subset(NoAr.one,time==finaltime)$n.broadleaf
+nB.NoAr.initial <- subset(NoAr.one,time==1)$n.broadleaf
+nB.Full.final <- subset(Full.one,time==finaltime)$n.broadleaf
+nB.Full.initial <- subset(Full.one,time==1)$n.broadleaf
+
+radA.NoFi.final <- subset(NoFi.one,time==finaltime)$circ.araucaria
+radA.NoFi.initial <- subset(NoFi.one,time==1)$circ.araucaria
+radA.NoAr.final <- subset(NoAr.one,time==finaltime)$circ.araucaria
+radA.NoAr.initial <- subset(NoAr.one,time==1)$circ.araucaria
+radA.Full.final <- subset(Full.one,time==finaltime)$circ.araucaria
+radA.Full.initial <- subset(Full.one,time==1)$circ.araucaria
+rad
+radB.NoFi.final <- subset(NoFi.one,time==finaltime)$circ.broadleaf
+radB.NoFi.initial <- subset(NoFi.one,time==1)$circ.broadleaf
+radB.NoAr.final <- subset(NoAr.one,time==finaltime)$circ.broadleaf
+radB.NoAr.initial <- subset(NoAr.one,time==1)$circ.broadleaf
+radB.Full.final <- subset(Full.one,time==finaltime)$circ.broadleaf
+radB.Full.initial <- subset(Full.one,time==1)$circ.broadleaf
+
+
+## array of hypotheses
+
 
 
 ### full plots
 plot.fours.columns(data,function(d,x) lines.par(d,x,"shade.threshold.ratio",colors))
-
-avmaxvalues <- aggregate(maxvalues,by=list(shade.threshold.ratio=maxvalues$shade.threshold.ratio),FUN=mean)
-avfinalvalues <- aggregate(finalvalues,by=list(shade.threshold.ratio=finalvalues$shade.threshold.ratio),FUN=mean)
-my_LHS_pars$data <- avmaxvalues[,par.names]
-
-my_LHS <- tell(my_LHS_pars, avmaxvalues[,my_outputnames])
--- headless/plotsfuns.utils.R
+++ headless/plotsfuns.utils.R
@@ -1,5 +1,5 @@
 
-lines.par <- function(data,column.data, column.par,colors) {
+lines.par <- function(data,column.data, column.par,colors = make_colors(data, column.par)) {
     ymax <- max(data[,column.data])
     xmax <- max(data$time)
     plot(NULL,NULL,ylim=c(0,ymax),xlim=c(0,xmax),xlab="Time",ylab=column.data)
@@ -21,15 +21,36 @@ lines.each <- function(data,column.data,color) {
     }
 }
 
-plot.fours.columns <- function(data, fun, label = "over time") {
+plot.fours.columns <- function(data, fun, label = "over time", ...) {
     par(mfrow=c(2,2))
-    fun(data,"n.broadleaf")
+    fun(data,"n.broadleaf",ylab="Population",...)
     title(paste0("Broadleaved population ",label))
-    fun(data,"n.araucaria")
+    fun(data,"n.araucaria",ylab="Population",...)
     title(paste0("Araucaria population ",label))
-    fun(data,"circ.broadleaf")
+    fun(data,"circ.broadleaf",ylab="Radius",...)
     title(paste0("Broadleaf radius ",label))
-    fun(data,"circ.araucaria")
+    fun(data,"circ.araucaria",ylab="Radius",...)
     title(paste0("Araucaria radius ",label))
     par(mfrow=c(1,1))
+}
+
+make_colors <- function(data,column.par){
+    data <- data[order(data[,column.par]),]
+    params <- sort(unique(data[,column.par]))
+    numpars <- length(params)
+    colors <- colorRampPalette(c("darkblue","red"))(numpars)
+    names(colors) <- params
+    colors
+}
+
+
+plot.one.timestep <- function(d,y,par,colors,...) plot(d[,y]~d[,par],col=colors[as.character(d[,par])],...);
+
+
+plot_final_values <- function(data,column.par) {
+    colors <- make_colors(data,column.par)
+    finaltime <- max(data$time)
+    finalvalues<- subset(data,time==finaltime)
+    label <- paste0("after ",finaltime," years")
+    plot.fours.columns(finalvalues,plot.one.timestep,label,par=column.par,xlab=column.par,colors=colors)
 }
