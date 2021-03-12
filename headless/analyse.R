c8370e946a2a122a02b5a50afc3b99d15dee1aea
M Salles <malicsalles@gmail.com>
2021-03-12 17:04:10 -0300

    hypotheses

--- headless/analyse.R
+++ headless/analyse.R
@@ -10,51 +10,48 @@ data <- subset(datafull, shade.threshold.ratio > 0)
 
 plot_final_values(data,"wildfire_rate")
 
-## get one parameter set
-parcols <- 6:13 ## cols with parameter values
-NoArdata <- subset(data,initial_pop_araucaria==0)
-NoFidata <- subset(data,wildfire_rate==0)
-Fulldata <- subset(data,initial_pop_araucaria>0 & wildfire_rate >0)
 
-NoFi.one <- subset(NoFidata,sim_unique_id==NoFidata$sim_unique_id[1])
-NoAr.one <- subset(NoArdata, shade_threshold_ratio == NoFi.one$shade_threshold_ratio[1])
-Full.one <- subset(Fulldata, shade_threshold_ratio == NoFi.one$shade_threshold_ratio[1])
+test.hypotheses <- function(data) {
+    ## get one parameter set
+    parcols <- 9:13 ## cols with parameter values
+    NoArdata <- subset(data,initial_pop_araucaria==0)
+    NoFidata <- subset(data,wildfire_rate==0)
+    Fulldata <- subset(data,initial_pop_araucaria>0 & wildfire_rate >0)
 
-## extract variables
-finaltime <- max(data$time)
+    NoFi.one <- subset(NoFidata,sim_unique_id==unique(NoFidata$sim_unique_id)[4])
+    NoAr.one <- subset(NoArdata, shade_threshold_ratio == NoFi.one$shade_threshold_ratio[1])
+    Full.one <- subset(Fulldata, shade_threshold_ratio == NoFi.one$shade_threshold_ratio[1])
 
-nA.NoFi.final <- subset(NoFi.one,time==finaltime)$n.araucaria
-nA.NoFi.initial <- subset(NoFi.one,time==1)$n.araucaria
-nA.NoAr.final <- subset(NoAr.one,time==finaltime)$n.araucaria
-nA.NoAr.initial <- subset(NoAr.one,time==1)$n.araucaria
-nA.Full.final <- subset(Full.one,time==finaltime)$n.araucaria
-nA.Full.initial <- subset(Full.one,time==1)$n.araucaria
+    ## extract variables
+    finaltime <- max(data$time)
 
-nB.NoFi.final <- subset(NoFi.one,time==finaltime)$n.broadleaf
-nB.NoFi.initial <- subset(NoFi.one,time==1)$n.broadleaf
-nB.NoAr.final <- subset(NoAr.one,time==finaltime)$n.broadleaf
-nB.NoAr.initial <- subset(NoAr.one,time==1)$n.broadleaf
-nB.Full.final <- subset(Full.one,time==finaltime)$n.broadleaf
-nB.Full.initial <- subset(Full.one,time==1)$n.broadleaf
+    NoFi.final <- subset(NoFi.one,time==finaltime)
+    NoFi.initial <- subset(NoFi.one,time==1)
+    NoAr.final <- subset(NoAr.one,time==finaltime)
+    NoAr.initial <- subset(NoAr.one,time==1)
+    Full.final <- subset(Full.one,time==finaltime)
+    Full.initial <- subset(Full.one,time==1)
 
-radA.NoFi.final <- subset(NoFi.one,time==finaltime)$circ.araucaria
-radA.NoFi.initial <- subset(NoFi.one,time==1)$circ.araucaria
-radA.NoAr.final <- subset(NoAr.one,time==finaltime)$circ.araucaria
-radA.NoAr.initial <- subset(NoAr.one,time==1)$circ.araucaria
-radA.Full.final <- subset(Full.one,time==finaltime)$circ.araucaria
-radA.Full.initial <- subset(Full.one,time==1)$circ.araucaria
-rad
-radB.NoFi.final <- subset(NoFi.one,time==finaltime)$circ.broadleaf
-radB.NoFi.initial <- subset(NoFi.one,time==1)$circ.broadleaf
-radB.NoAr.final <- subset(NoAr.one,time==finaltime)$circ.broadleaf
-radB.NoAr.initial <- subset(NoAr.one,time==1)$circ.broadleaf
-radB.Full.final <- subset(Full.one,time==finaltime)$circ.broadleaf
-radB.Full.initial <- subset(Full.one,time==1)$circ.broadleaf
+    ## hyp1 : patch will grow
+    hyp1 <- max(Full.final$circ.araucaria,Full.final$circ.broadleaf) > max(Full.initial$circ.araucaria,Full.initial$circ.broadleaf)
+    ## hyp2 : araucaria is on the edge
+    hyp2 <- Full.final$circ.araucaria > Full.final$circ.broadleaf
+    ## hyp3 : broadleaf grows better with araucaria / can't grow without araucaria
+    hyp3a <- Full.final$n.broadleaf > NoAr.final$n.broadleaf
+    hyp3b <- NoAr.final$circ.broadleaf <= NoAr.final$circ.broadleaf
+    ## hyp4 : without fire, broadleaf grows and competes with araucaria
+    hyp4a <- Full.final$n.broadleaf < NoFi.final$n.broadleaf
+    hyp4b <- Full.final$n.araucaria > NoFi.final$n.araucaria
+    ## hyp5 : broadleaf expansion tracks araucaria expansion
+    hyp5 <- NA
+    ## extinction full check
+    ext <- max(Full.final$circ.araucaria,Full.final$circ.broadleaf) ==0
 
-
-## array of hypotheses
-
-
+    ## array of hypotheses
+    hypotheses <- c(hyp1,hyp2,hyp3a,hyp3b,hyp4a,hyp4b,hyp5,ext)
+    names(hypotheses) <- c("1","2","3a","3b","4a","4b","5","full extinction")
+    hypotheses
+}
 
 ### full plots
 plot.fours.columns(data,function(d,x) lines.par(d,x,"shade.threshold.ratio",colors))
