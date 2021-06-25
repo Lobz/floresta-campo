85b67e23cfc8cbeb3530186b9d921a8fd55c31e4
M Salles <malicsalles@gmail.com>
2021-06-25 17:01:59 -0300

    organizing createxml

--- headless/createxml.utils.R
+++ headless/createxml.utils.R
@@ -10,16 +10,69 @@ simheadbeg <- '
 simheadend <- paste0('" sourcePath="',gamlfile,'" finalStep="',finalstep,'" experiment="fireandforest" until="length(araucaria) + length(broadleaf) = 0 or max(rad_broadleaf.y, rad_araucaria.y) > landscape_size/2">')
 footer <- '</Experiment_plan>'
 
-w <- function (str) {
-    write(str,file=my_filename,append=T)
-}
-
 par.line <- function(name,p) {
-  paste0('<Parameter name="',name,'" type="FLOAT" value="',p,'" />')
+    paste0('<Parameter name="',name,'" type="FLOAT" value="',p,'" />')
 }
 
 par.row <- function(row) {
-  paste(par.line(names(row),row),collapse="\n")
+    paste(par.line(names(row),row),collapse="\n")
 }
 
 chars <- c(LETTERS,letters,0:9)
+
+
+### MAIN FUNCTION
+
+createxml <- function(par.data, groupname, scenarios=TRUE) {
+    chunksize <- 10
+
+    ### Parameters
+    sim.params<-apply(par.data,1,par.row)
+    ## scenarios
+    if (scenarios) {
+        FMparams <- paste0(sim.params,'')
+        NAparams <- paste0(sim.params,'<Parameter name="initial_pop_ratio" type="INT" value="0" />')
+        NFparams <- paste0(sim.params,'<Parameter name="Wildfires" type="BOOLEAN" value="false" />')
+
+        my_names <- c("Full","NoAr","NoFi")
+        sim.params <- c(FMparams,NAparams,NFparams)
+    }
+
+    ## UNIQUE IDS
+    n <- length(sim.params)
+    rndc <- do.call(paste0, replicate(5, sample(chars, n , TRUE), FALSE)) # generate unique ids
+    sim_ids <- paste0(groupname,'_',rndc)  ## array of sim ids
+    simheaders <- paste0(simheadbeg,sim_ids,simheadend) ## array of headers
+
+    ## JOIN ALL SIM INFO
+    simxml <- paste(simheaders,sim.params,outputs,'</Simulation>\n', sep="\n")
+
+    ### SPLIT INTO CHUNKS
+    chunks <- split (simxml, ceiling(seq_along(simxml)/chunksize))
+
+    my_filenames <- paste0("xmls/",groupname,"_chunk_",1:length(chunks),".xml")
+    ### WRITING
+    w <- function (str) {
+        write(str,file=my_filename,append=T)
+    }
+
+    for (i in 1:length(chunks)) {
+        my_filename <- my_filenames[i]
+        simxml <- chunks[[i]]
+
+        file.create(my_filename)
+        write(header, my_filename, append=FALSE)
+        sapply(simxml,FUN=w)
+        w(footer)
+    }
+
+    return(my_filenames)
+}
+
+## RUNNING
+
+run_simulations <- function(my_filenames, outputdir) {
+    for (filename in my_filenames) {
+        system(paste0('gama-headless.bat ',filename,' ',outputdir))
+    }
+}
