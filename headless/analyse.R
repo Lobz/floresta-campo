b769512f7c70c901193df35dc82547d2b5e52efe
M Salles <malicsalles@gmail.com>
2022-02-18 17:14:47 -0300

    images

--- headless/analyse.R
+++ headless/analyse.R
@@ -16,6 +16,16 @@ statistics_full_minus_noAr <- statistics_full - statistics_noAr
 statistics_noFi_minus_noAr <- statistics_noFi - statistics_noAr
 statistics_full_minus_noFi <- statistics_full - statistics_noFi
 
+## extract image files
+imagedir <- "./data/scenarios_Dec232021_kv20u-out/scenarios_Dec232021_kv20u-out/snapshot/"
+fullimages<- as.character(subset(data,full & !is.na(image_file))$image_file)
+nofiimages<- as.character(subset(data,noFi & !is.na(image_file))$image_file)
+noarimages<- as.character(subset(data,noAr & !is.na(image_file))$image_file)
+
+file.rename(paste0(imagedir,fullimages),paste0(imagedir,'/full/',fullimages))
+file.rename(paste0(imagedir,nofiimages),paste0(imagedir,'/nofi/',nofiimages))
+file.rename(paste0(imagedir,noarimages),paste0(imagedir,'/noar/',noarimages))
+
 
 finalvalues <- get_finalsteps(data)
 summary(finalvalues)
